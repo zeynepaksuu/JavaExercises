e0868feb2958148ee96895a33ad6b72fd8ae73a1
Zeynep Aksu <zeynepgspor@gmail.com>
2025-07-24 15:06:25 +0300

    loop example added

---
+++ src/Homeworks/PerfectNumberCalculation.java
@@ -0,0 +1,24 @@
+package Homeworks;
+import java.util.Scanner;
+
+public class PerfectNumberCalculation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+        if (sum == number) {
+            System.out.println(number + " is a perfect number.");
+        } else {
+            System.out.println(number + " is not a perfect number.");
+        }
+    }
+}
