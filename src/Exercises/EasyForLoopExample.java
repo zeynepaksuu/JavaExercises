82cf5fc278f2b40d87d7f33a82538ecd019da68e
Zeynep Aksu <zeynepgspor@gmail.com>
2025-07-25 20:35:20 +0300

    basic loop example

---
+++ src/Exercises/EasyForLoopExample.java
@@ -0,0 +1,13 @@
+package Exercises;
+
+public class EasyForLoopExample {
+    public static void main (String[] args) {
+
+        int sum = 0;
+        for(int i=0; i < 100; i++){
+            sum += i;
+        }
+        System.out.print("Sum: " +sum);
+    }
+
+}
